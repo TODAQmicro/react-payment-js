a7bcaa973b4a0d3e0882ebfbf422da6d08a7b699
Matthew Mihok <mihok@identbase.io>
2024-03-24 08:06:10 -0400

    feat: Working payment components

--- lib/esm/types/components/PaymentElement.d.ts
+++ lib/esm/types/components/PaymentElement.d.ts
@@ -3,6 +3,7 @@ export interface ElementProps {
     id?: string;
     className?: string;
     options?: any;
+    onPayment?: () => void;
 }
 export declare const PaymentElement: FunctionComponent<ElementProps>;
 //# sourceMappingURL=PaymentElement.d.ts.map
--- src/components/PaymentElement.tsx
+++ src/components/PaymentElement.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FunctionComponent, useLayoutEffect, useRef } from "react";
+import { FunctionComponent, useEffect, useLayoutEffect, useRef } from "react";
 import { MicroElement } from "@todaqmicro/payment-js";
 
 import { useElements } from "..";
@@ -9,12 +9,26 @@ export interface ElementProps {
   id?: string;
   className?: string;
   options?: any; // TODO(mihok): Type this properly.
+  onPayment?: () => void;
 }
 
-export const PaymentElement: FunctionComponent<ElementProps> = ({ id, className, options }) => {
+export const PaymentElement: FunctionComponent<ElementProps> = ({ id, className, options, onPayment }) => {
   const ctx = useElements();
   const elements = ctx && 'elements' in ctx ? ctx.elements : null;
   const node = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    let mounted = true;
+
+    if (mounted) {
+      document.addEventListener('purchase', () => {
+        // TODO(mihok): Add in validation logic
+        onPayment && onPayment();
+      });
+    }
+
+    return () => { mounted = false; };
+  }, []);
 
   useLayoutEffect(() => {
     if (node.current !== null && elements) {
